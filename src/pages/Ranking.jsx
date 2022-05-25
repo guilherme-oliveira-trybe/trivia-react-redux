46631c1bbbb622bb40aab43a0ff3b7fd511835da
Michele Gusmao <michele.gusmao7@gmail.com>
2022-05-25 18:07:48 -0300

    Adiciona requisito 18 e arquivo lock-json

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
 class Ranking extends Component {
+  gotToHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <h2 data-testid="ranking-title">Ranking</h2>
+      <div>
+        <h2 data-testid="ranking-title">Ranking</h2>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.goToHome }
+        >
+          Home
+        </button>
+      </div>
+
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
