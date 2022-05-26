63beea1c3169b7f19213e5ebbafb1b9ad8d8239d
Jazyel99 <jaziel@outlook.com>
2022-05-26 00:16:12 -0300

    feat(): adicionado funcionalidade para fazer a parada do timer e o reset, ao clicar nos botões de next ou de respostas.

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,56 +1,79 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { timer, disabled } from '../actions';
+import { timer, disabled as diabledAction } from '../actions';
 
 class Timer extends Component {
   constructor() {
     super();
-    this.state = {
-      seconds: 30,
-    };
+    // this.state = {
+    //   seconds: 30,
+    // };
 
     this.idInterval = null;
   }
 
   componentDidMount() {
-    this.startTimer();
+    const FIVE_SECONDS_IN_MILLESECONDS = 5000;
+    setTimeout(() => {
+      this.startTimer();
+    }, FIVE_SECONDS_IN_MILLESECONDS);
   }
 
   componentDidUpdate() {
-    const { seconds } = this.state;
-    const { dispatch } = this.props;
-    if (seconds === 0) {
+    // const { seconds } = this.state;
+    const { stop } = this.props;
+    if (stop) {
       this.stopTimer();
     }
-    dispatch(timer(seconds));
+    // dispatch(timer(seconds));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.idInterval);
   }
 
   startTimer = () => {
     const milisegundos = 1000;
+    const { dispatch } = this.props;
     this.idInterval = setInterval(() => {
-      this.setState((prevState) => ({
-        seconds: prevState.seconds - 1,
-      }));
+      // this.setState((prevState) => ({
+      //   seconds: prevState.seconds - 1,
+      // }));
+      const { seconds } = this.props;
+      if (seconds > 0) {
+        dispatch(timer(seconds - 1));
+      }
+      if (seconds === 0) this.stopTimer();
     }, milisegundos);
   }
 
   stopTimer = () => {
     const { dispatch } = this.props;
     clearInterval(this.idInterval);
-    dispatch(disabled(true));
+    dispatch(diabledAction(true));
   }
 
   render() {
-    const { seconds } = this.state;
+    // const { seconds } = this.state;
+    const { seconds } = this.props;
+    const secondsInString = seconds.toString();
+
     return (
-      <span>{ seconds }</span>
+      <span>{ secondsInString.length === 2 ? seconds : `0${seconds}` }</span>
     );
   }
 }
 
 Timer.propTypes = {
   dispatch: PropTypes.func,
+  stop: PropTypes.bool,
 }.isRequired;
 
-export default connect()(Timer);
+const mapStateToProps = (state) => ({
+  seconds: state.timer.timer,
+});
+
+export default connect(mapStateToProps)(Timer);
+
+// export default connect()(Timer);
