7c1843b3f3f3126d719b902f105b74ad58210201
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-05-30 15:48:12 -0300

    Requisito 20

---
+++ src/tests/mocks/index.js
@@ -0,0 +1,14 @@
+const players = [
+  {
+    name: 'Guilherme',
+    picture: 'https://www.gravatar.com/avatar/8292a81cf09a02db5ccefac6b27cb95d',
+    score: 100,
+  },
+  {
+    name: 'teste',
+    picture: 'https://www.gravatar.com/avatar/10b9231267e5844f485572e2c3ae14b1',
+    score: 99,
+  }
+];
+
+export default players;
